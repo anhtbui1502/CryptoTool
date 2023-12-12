f191a49bddc847e22357f9eff2371de965484f49
anhtbui1502 <anh.tbui1502@gmail.com>
2023-12-12 08:44:21 +0700

    Update bybit_api.py

--- bybit_api.py
+++ bybit_api.py
@@ -1,5 +1,8 @@
 import json
+
+from pandas import DataFrame
 from pybit.unified_trading import HTTP
+import pandas as pd
 
 def create_connection() -> HTTP:
     #main net
@@ -11,6 +14,78 @@ def create_connection() -> HTTP:
     session = HTTP(api_key=key, api_secret=secret, testnet=True)
     return session
 
-def get_current_position():
+def get_current_position() -> DataFrame:
     session = create_connection()
-    print(session.get_positions(category="linear", settleCoin="USDT"))
+    response=session.get_positions(category="linear", settleCoin="USDT")
+    print(response)
+    result=format_data(response)
+    print(result)
+    return result
+	
+def get_current_order() -> DataFrame:
+	session = create_connection()
+    response=session.get_open_orders(category="linear", settleCoin="USDT",  openOnly=0, limit=1)
+    print(response)
+    result=format_data(response)
+    print(result)
+    return result
+
+def format_data(response):
+    if response.get('retCode') != 0:
+        return
+    result = response.get('result', None)
+    if not result:
+        return
+
+    data = result.get('list', None)
+    if not data:
+        return
+
+    data = pd.DataFrame(data,
+        columns=[
+            'symbol',
+            'side',
+            "createdTime",
+            "leverage",
+            "avgPrice",
+            "markPrice",
+            "positionValue",
+            "unrealisedPnl"
+        ],
+        )
+
+    return data
+	
+def calcel_order(symbol, order_id):
+	session = create_connection()
+    response=session.cancel_order(category="linear", symbol=symbol, orderId=order_id)
+    print(response)
+	
+def place_order(symbol, side, price, qty, tp, sl, leverage):
+	session = create_connection()
+    response=session.place_order(
+		category="linear",
+		symbol=symbol,
+		side=side,
+		orderType="Limit",
+		qty=qty,
+		price=price,
+		isLeverage=leverage,
+		takeProfit=tp,
+		stopLoss=sl
+	)
+    print(response)
+	
+def update_order(symbol, order_link_id, tp, sl):
+	session = create_connection()
+    response=session.amend_order(
+		category="linear",
+		symbol=symbol,
+		orderLinkId=order_link_id,
+		takeProfit=tp,
+		stopLoss=sl
+	)
+    print(response)
+
+
+
